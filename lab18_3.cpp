8ed1d7b11eaa968e395154e7d4471f747f5389fc
earthx15 <uncles1512@hotmail.com>
2021-02-23 17:46:26 +0700

    pass

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+	string name; 
+	int id;
+	char gender;
+	double gpa;
 };
 
 struct course{
@@ -20,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ 
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -62,11 +63,12 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
-		allstudents.push_back(s); 		
+		student s; char n[100]; 
+		char format[]="%[^,],%d,%c,%lf";
+        sscanf(textline.c_str(),format,n,&s.id,&s.gender,&s.gpa);
+		s.name=n;
+		// cout << s.id <<"\n";
+        allstudents.push_back(s); 		
 	}
 	
 	int state = 1;
@@ -83,7 +85,7 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
+				allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -92,7 +94,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
